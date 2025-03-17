297db0e8ebee53b1ee91c4705f0ac5d65bd9a44c
chiangyiyang <chiangyiyang@gmail.com>
2025-03-17 15:27:27 +0800

    add CORS and Items API

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
 // 中介軟體 (Middleware)，用來解析 JSON 請求
 app.use(express.json());
+app.use(cors())
+
 
 // 定義 API 路由
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello, World!" });
+  res.json({ message: "Hello, 123!" });
+});
+
+// 定義 我的API 路由
+const my_data = [
+  {
+    "longitude": 120.5646,
+    "latitude": 25.0335,
+    "height": 150,
+    "heading": 0,
+    "length": 500
+  }
+]
+
+app.get("/items", (req, res) => {
+  res.json(my_data);
+  console.log("GET Items");
 });
 
 // 啟動伺服器
